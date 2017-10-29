77e7b954151d9ab8e6f02448a4eea279e498a9b8
Serg Kudymov <sergkudimov@gmail.com>
2017-10-30 00:05:00 +0500

    account manager added

---
+++ WebApplicationASU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationASU.Startup))]
+namespace WebApplicationASU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
